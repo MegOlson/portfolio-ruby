e7c600948e2b9560e08d7749c4f6fdf1083bd64e
mo <meganannetteolson@yahoo.com>
2017-11-11 13:28:57 -0800

    Add styling and new picture to home page.

--- app.rb
+++ app.rb
@@ -3,18 +3,26 @@ require 'sinatra/reloader'
 also_reload('lib/**/*.rb')
 require 'pry'
 
-get('/') do
+get '/' do
   erb(:index)
 end
 
-get('/about') do
+get '/about' do
   erb(:about)
 end
 
-get('/portfolio') do
+get '/portfolio' do
   erb(:portfolio)
 end
 
-get('/contact') do
+get '/contact' do
   erb(:contact)
+end
+
+get '/ruby' do
+  erb(:ruby)
+end
+
+get '/javascript' do
+  erb(:javascript)
 end
