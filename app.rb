36ce07cba61dc04e367db1155f355a3c3e4f3cde
mo <meganannetteolson@yahoo.com>
2017-11-10 21:04:04 -0800

    Begin portfolio.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload('lib/**/*.rb')
+require 'pry'
+
+get('/') do
+  erb(:index)
+end
+
+get('/about') do
+  erb(:about)
+end
+
+get('/portfolio') do
+  erb(:portfolio)
+end
+
+get('/contact') do
+  erb(:contact)
+end
